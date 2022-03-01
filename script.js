19965e696712fd9a0c2470717a3bed54ad264fb7
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-01 12:16:42 +0200

    Add popup div

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-const mobileMenu = document.querySelector('#menu');
-const listItem = document.querySelectorAll('.hidden-items');
-const nav = document.querySelector('nav');
-const navName = document.querySelector('#name');
-const menuList = document.querySelector('nav ul');
-const exitIcon = document.querySelector('.exit');
-const envelope = document.querySelector('#envelope');
+const mobileMenu = document.querySelector("#menu");
+const listItem = document.querySelectorAll(".hidden-items");
+const nav = document.querySelector("nav");
+const navName = document.querySelector("#name");
+const menuList = document.querySelector("nav ul");
+const exitIcon = document.querySelector(".exit");
+const envelope = document.querySelector("#envelope");
+const btn = document.querySelectorAll("#projects button");
+const projectsSection = document.querySelector("#projects");
+
+/* Menu */
 
 function resetMobileMenu() {
-  listItem.forEach((item) => item.classList.remove('show'));
-  nav.classList.remove('expand');
-  navName.classList.remove('hidden');
-  menuList.classList.remove('ul-list');
-  mobileMenu.classList.remove('hidden');
-  exitIcon.classList.remove('X');
-  envelope.classList.remove('hidden');
+  listItem.forEach((item) => item.classList.remove("show"));
+  nav.classList.remove("expand");
+  navName.classList.remove("hidden");
+  menuList.classList.remove("ul-list");
+  mobileMenu.classList.remove("hidden");
+  exitIcon.classList.remove("X");
+  envelope.classList.remove("hidden");
 }
 
-mobileMenu.addEventListener('click', () => {
-  listItem.forEach((item) => item.classList.add('show'));
-  nav.classList.add('expand');
-  navName.classList.add('hidden');
-  menuList.classList.add('ul-list');
-  mobileMenu.classList.add('hidden');
-  exitIcon.classList.add('X');
-  envelope.classList.add('hidden');
+mobileMenu.addEventListener("click", () => {
+  listItem.forEach((item) => item.classList.add("show"));
+  nav.classList.add("expand");
+  navName.classList.add("hidden");
+  menuList.classList.add("ul-list");
+  mobileMenu.classList.add("hidden");
+  exitIcon.classList.add("X");
+  envelope.classList.add("hidden");
 
-  listItem.forEach((item) => item.addEventListener('click', resetMobileMenu));
+  listItem.forEach((item) => item.addEventListener("click", resetMobileMenu));
 });
 
-exitIcon.addEventListener('click', resetMobileMenu);
+exitIcon.addEventListener("click", resetMobileMenu);
 
 function resizeWindow() {
   if (window.innerWidth > 600) {
@@ -36,4 +40,23 @@ function resizeWindow() {
   }
 }
 
-window.addEventListener('resize', resizeWindow);
+window.addEventListener("resize", resizeWindow);
+
+/* Project section */
+
+const projects = [
+  {
+    title: "Multi-Post Stories",
+  },
+];
+
+function createInfo() {}
+
+function createDiv() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "popup");
+  projectsSection.appendChild(div);
+  createInfo();
+}
+
+btn.forEach((button) => button.addEventListener("click", createDiv));
