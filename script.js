c147dc169cd317073cca5e4894cfeda0adc7590e
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-02 22:10:36 +0200

    Modify popup functionality and add styles

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-const mobileMenu = document.querySelector("#menu");
-const listItem = document.querySelectorAll(".hidden-items");
-const nav = document.querySelector("nav");
-const navName = document.querySelector("#name");
-const menuList = document.querySelector("nav ul");
-const exitIcon = document.querySelector(".exit");
-const envelope = document.querySelector("#envelope");
-const btn = document.querySelectorAll("#projects button");
-const projectsSection = document.querySelector("#projects");
+const mobileMenu = document.querySelector('#menu');
+const listItem = document.querySelectorAll('.hidden-items');
+const nav = document.querySelector('nav');
+const navName = document.querySelector('#name');
+const menuList = document.querySelector('nav ul');
+const exitIcon = document.querySelector('.exit');
+const envelope = document.querySelector('#envelope');
+const btn = document.querySelectorAll('#projects button');
+const projectsSection = document.querySelector('#projects');
+const blurProjects = document.querySelectorAll('#projects > div');
 
 /* Menu */
 
 function resetMobileMenu() {
-  listItem.forEach((item) => item.classList.remove("show"));
-  nav.classList.remove("expand");
-  navName.classList.remove("hidden");
-  menuList.classList.remove("ul-list");
-  mobileMenu.classList.remove("hidden");
-  exitIcon.classList.remove("X");
-  envelope.classList.remove("hidden");
+  listItem.forEach((item) => item.classList.remove('show'));
+  nav.classList.remove('expand');
+  navName.classList.remove('hidden');
+  menuList.classList.remove('ul-list');
+  mobileMenu.classList.remove('hidden');
+  exitIcon.classList.remove('X');
+  envelope.classList.remove('hidden');
 }
 
-mobileMenu.addEventListener("click", () => {
-  listItem.forEach((item) => item.classList.add("show"));
-  nav.classList.add("expand");
-  navName.classList.add("hidden");
-  menuList.classList.add("ul-list");
-  mobileMenu.classList.add("hidden");
-  exitIcon.classList.add("X");
-  envelope.classList.add("hidden");
+mobileMenu.addEventListener('click', () => {
+  listItem.forEach((item) => item.classList.add('show'));
+  nav.classList.add('expand');
+  navName.classList.add('hidden');
+  menuList.classList.add('ul-list');
+  mobileMenu.classList.add('hidden');
+  exitIcon.classList.add('X');
+  envelope.classList.add('hidden');
 
-  listItem.forEach((item) => item.addEventListener("click", resetMobileMenu));
+  listItem.forEach((item) => item.addEventListener('click', resetMobileMenu));
 });
 
-exitIcon.addEventListener("click", resetMobileMenu);
+exitIcon.addEventListener('click', resetMobileMenu);
 
 function resizeWindow() {
   if (window.innerWidth > 600) {
@@ -40,45 +41,82 @@ function resizeWindow() {
   }
 }
 
-window.addEventListener("resize", resizeWindow);
+window.addEventListener('resize', resizeWindow);
 
 /* Project section */
 
-const projects = [
-  
-];
-
-
-function createInfo(button) {
-  let object = {};
-  // const projetsTitles = document.querySelectorAll('[data-title]');
-  // const projetPragraph = document.querySelectorAll('[data-p]');
-  const projetsTitle = document.querySelector(`${button.dataset.btn} [data-title]`)
-  const projetPragraph = document.querySelector(`${button.dataset.btn} [data-p]`);
-  // object.title =  `${projetsTitle.textContent}`,
-  // object.pragraph =  `${projetPragraph.innerHTML}`;
-  object.title = projetsTitle;
-  object.pragraph = projetPragraph;
-  projects.push(object);
-  console.log(object);
-  console.log(projects);
-  createDiv();
-}
+const projects = [];
 
 function createDiv() {
-  let div = document.createElement("div");
-  div.setAttribute("class", "popup");
-  // div.innerHTML = `<p>${projects[0].title}</p>`;
-  // div.innerHTML = `<p>${projects[0].pragraph}</p>`;
-  for (let items of Object.values(projects[0])) {
-    console.log(items);
-    div.appendChild(items);
+  const div = document.createElement('div');
+  const description = document.createElement('p');
+  const liveSiteBtn = document.createElement('button');
+  const sourceBtn = document.createElement('button');
+  const projectExitBtn = document.createElement('button');
+  description.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.";
+  description.classList.add('popup-paragraph');
+  liveSiteBtn.textContent = 'See Live';
+  sourceBtn.textContent = 'See Source';
+  liveSiteBtn.classList.add('popup-btn', 'live');
+  sourceBtn.classList.add('popup-btn', 'popup-margin', 'source');
+  projectExitBtn.style.backgroundImage = "URL('icons/Icon.png')";
+  projectExitBtn.style.backgroundRepeat = 'no-repeat';
+  projectExitBtn.style.backgroundSize = 'cover';
+  projectExitBtn.classList.add('popup-exit');
+  div.classList.toggle('popup');
+  if (window.innerWidth < 900) {
+    div.classList.toggle('mobile-width');
+    description.classList.toggle('popup-mobile-p');
+    sourceBtn.classList.toggle('mobile-btn');
+    liveSiteBtn.classList.toggle('mobile-btn');
   }
+  Object.values(projects[0]).forEach((item) => div.appendChild(item));
+
+  div.appendChild(description);
+  div.appendChild(liveSiteBtn);
+  div.appendChild(sourceBtn);
+  div.appendChild(projectExitBtn);
   projectsSection.appendChild(div);
+  projectExitBtn.addEventListener('click', () => {
+    div.remove();
+    projects.pop();
+    document.body.classList.toggle('no-scroll');
+    document.body.style.background = '#fff';
+    blurProjects.forEach((project) => project.classList.toggle('blur'));
+  });
+}
+
+function createInfo(button) {
+  const object = {};
+  const projectsTitle = document
+    .querySelector(`${button.dataset.btn} [data-title]`)
+    .cloneNode(true);
+  const projectsLanguages = document
+    .querySelector(`${button.dataset.btn} [data-language]`)
+    .cloneNode(true);
+  const projectsImages = document
+    .querySelector(`${button.dataset.btn} [data-img]`)
+    .cloneNode(true);
+  projectsTitle.classList.add('popup-title');
+  object.title = projectsTitle;
+  projectsLanguages.classList.add('popup-languages');
+  object.language = projectsLanguages;
+  projectsImages.classList.toggle('popup-img');
+  if (window.innerWidth < 900) {
+    projectsImages.classList.toggle('popup-mobile-img');
+    projectsTitle.classList.toggle('mobile-title');
+    projectsLanguages.classList.toggle('mobile-languages');
+  }
+  object.image = projectsImages;
+  projects.push(object);
+  createDiv();
 }
 
 btn.forEach((button) => {
   button.addEventListener('click', () => {
+    document.body.style.background = 'black';
     createInfo(button);
-  })
+    document.body.classList.toggle('no-scroll');
+    blurProjects.forEach((project) => project.classList.toggle('blur'));
+  });
 });
